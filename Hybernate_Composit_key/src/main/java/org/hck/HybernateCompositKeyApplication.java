378e4c11e343440c78bea569ade50d4e2759cc42
kranthi <kranthi@123.ij>
2021-04-16 12:00:29 +0530

    First Commit on thise application

---
+++ Hybernate_Composit_key/src/main/java/org/hck/HybernateCompositKeyApplication.java
@@ -0,0 +1,13 @@
+package org.hck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybernateCompositKeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HybernateCompositKeyApplication.class, args);
+	}
+
+}
---
+++ Hybernate_Composit_key/src/test/java/org/hck/HybernateCompositKeyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybernateCompositKeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
