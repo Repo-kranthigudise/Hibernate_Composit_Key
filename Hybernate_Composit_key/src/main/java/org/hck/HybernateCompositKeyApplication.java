5f4b938b67508983cab13fb63b0d0a63990a8697
kranthi <kranthi@123.ij>
2021-04-16 12:38:48 +0530

    #ComponentScann Added for scann all comonents

--- Hybernate_Composit_key/src/main/java/org/hck/HybernateCompositKeyApplication.java
+++ Hybernate_Composit_key/src/main/java/org/hck/HybernateCompositKeyApplication.java
@@ -2,8 +2,10 @@ package org.hck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"org.hk.*"})
 public class HybernateCompositKeyApplication {
 
 	public static void main(String[] args) {
